b1c636cd0615a9031aa5d94e48dbec0ffff9a350
Ryan Roemer <ryan@loose-bits.com>
2015-10-22 10:30:59 -0700

    Add environment and base abstraction starts.

--- bin/builder.js
+++ bin/builder.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log("TODO: IMPLEMENT CLI", process.argv);
+// Set up environment
+var Environment = require("../lib/environment");
+var env = new Environment();
+env.updatePath();
+
+// Infer task to run
+var Task = require("../lib/task");
+var task = new Task();
+var cmd = task.getCommand();
+
+// Run the task
+// TODO: Decompose to `lib/runner.js`
+var exec = require("child_process").exec;
+var proc = exec(cmd, {
+  env: env.env
+}, function (err) {
+  if (err) { process.exit(err.code); }
+});
+
+proc.stdout.pipe(process.stdout, { end: false });
+proc.stderr.pipe(process.stderr, { end: false });
