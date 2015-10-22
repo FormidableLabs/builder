f413f6e7dfdaa11cc06e56d1d155a831fcc2087c
Ryan Roemer <ryan@loose-bits.com>
2015-10-22 16:53:28 -0700

    Add logging.

--- bin/builder.js
+++ bin/builder.js
@@ -13,18 +13,19 @@ var cmd = task.getCommand();
 // Run the task
 // TODO: Decompose to `lib/runner.js`
 var exec = require("child_process").exec;
-
-// TODO: Integrate a real logger with colors.
-console.log(task.toString());
-console.log("[builder:exec] " + cmd);
-
 var proc = exec(cmd, {
   env: env.env
 }, function (err) {
   if (err) {
+    log.error("error", "Failure: " + cmd);
     process.exit(err.code || 1);
   }
 });
 
 proc.stdout.pipe(process.stdout, { end: false });
 proc.stderr.pipe(process.stderr, { end: false });
+
+// TODO: Move around logger.
+var log = require("../lib/log");
+log.info("task", task.toString());
+log.info("exec", cmd);
