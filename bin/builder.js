9db29c2ff03465e9788177febd0bd38c5871a116
Ryan Roemer <ryan@loose-bits.com>
2015-10-24 15:05:25 -0700

    Add in concurrent implementation.

--- bin/builder.js
+++ bin/builder.js
@@ -12,19 +12,8 @@ env.updateNodePath("node_modules/builder-react-component/node_modules");
 // Infer task to run
 var Task = require("../lib/task");
 var task = new Task();
-log.info("task", task.toString());
-
-// Detect help
-if (task.action === "help") {
-  var tasks = require("../lib/scripts").display();
-  log.info("help",
-    "\n\nUsage: \n\n  builder [action] [task]" +
-    "\n\nActions: \n\n  " + task.ACTIONS.join(", ") +
-    "\n\nTasks: \n" + tasks);
-  process.exit(0);
-}
 
 // Run the task
-var runner = require("../lib/runner");
-var cmd = task.getCommand();
-runner.run(env, cmd);
+task.execute(env, function (err) {
+  process.exit(err ? err.code || 1 : 0);
+});
